fc65dc3ec7005f46bf68bbbc23c652f92316e386
Adam Drewery <adam.drewery@audacia.co.uk>
2019-10-22 15:20:56 +0100

    Add more unit tests.

--- Audacia.ExceptionHandling/Extensions.cs
+++ Audacia.ExceptionHandling/Extensions.cs
@@ -8,18 +8,32 @@ namespace Audacia.ExceptionHandling
 	/// <summary>Extension methods.</summary>
 	public static class Extensions
 	{
-		/// <summary>Register the default handler for a <see cref="KeyNotFoundException"/>.</summary>
-		public static ExceptionHandlerCollectionBuilder KeyNotFoundException(this ExceptionHandlerBuilder builder) => 
-			builder.Handle((KeyNotFoundException e) => new ErrorResult(HttpStatusCode.NotFound, e.Message));
-		
-		/// <summary>Register the default handler for a <see cref="UnauthorizedAccessException"/>.</summary>
-		public static ExceptionHandlerCollectionBuilder UnauthorizedAccessException(this ExceptionHandlerBuilder builder) => 
-			builder.Handle((UnauthorizedAccessException e) => new ErrorResult(HttpStatusCode.Unauthorized));
+		/// <summary>Register the default handler for a <see cref="System.Collections.Generic.KeyNotFoundException"/> with the specified HTTP status code.</summary>
+		public static ExceptionHandlerCollectionBuilder KeyNotFoundException(this ExceptionHandlerBuilder builder, HttpStatusCode statusCode) =>
+			builder.Handle((KeyNotFoundException e) => new ErrorResult(statusCode, e.Message));
+
+		/// <summary>Register the default handler for a <see cref="System.Collections.Generic.KeyNotFoundException"/> with the specified HTTP status code.</summary>
+		public static ExceptionHandlerCollectionBuilder KeyNotFoundException(this ExceptionHandlerBuilder builder, int statusCode) => builder.KeyNotFoundException((HttpStatusCode)statusCode);
+
+		/// <summary>Register the default handler for a <see cref="System.Collections.Generic.KeyNotFoundException"/>with an HTTP status code of 404: Not Found.</summary>
+		public static ExceptionHandlerCollectionBuilder KeyNotFoundException(this ExceptionHandlerBuilder builder) => builder.KeyNotFoundException(HttpStatusCode.NotFound);
+
+		/// <summary>Register the default handler for a <see cref="System.UnauthorizedAccessException"/> with the specified HTTP status code.</summary>
+		public static ExceptionHandlerCollectionBuilder UnauthorizedAccessException(this ExceptionHandlerBuilder builder, HttpStatusCode statusCode) =>
+			builder.Handle((UnauthorizedAccessException e) => new ErrorResult(statusCode));
+
+		/// <summary>Register the default handler for a <see cref="System.UnauthorizedAccessException"/> with the specified HTTP status code.</summary>
+		public static ExceptionHandlerCollectionBuilder UnauthorizedAccessException(this ExceptionHandlerBuilder builder, int statusCode) =>
+			builder.UnauthorizedAccessException((HttpStatusCode)statusCode);
+
+		/// <summary>Register the default handler for a <see cref="System.UnauthorizedAccessException"/> with an HTTP status code of 401: Unauthorized.</summary>
+		public static ExceptionHandlerCollectionBuilder UnauthorizedAccessException(this ExceptionHandlerBuilder builder) =>
+			builder.UnauthorizedAccessException(HttpStatusCode.Unauthorized);
 
 		/// <summary>Register a handler for the specified exception, using the specified transformation function.</summary>
 		public static ExceptionHandlerCollectionBuilder Handle<T>(
-			this ExceptionHandlerCollectionBuilder builder, 
-			Func<T, ErrorResult> handler) where T : Exception => 
+			this ExceptionHandlerCollectionBuilder builder,
+			Func<T, ErrorResult> handler) where T : Exception =>
 			builder.Handle.Handle(handler);
 	}
 }
