647d3bdc2454268284e333a325869b692763e8b0
liam.ward <liam.ward@audacia.co.uk>
2020-11-13 09:05:39 +0000

    `IHttpExceptionHandler` inherits from `IExceptionHandler`
    Signed-off-by: liam.ward <liam.ward@audacia.co.uk>

--- src/Audacia.ExceptionHandling/Handlers/IHttpExceptionHandler.cs
+++ src/Audacia.ExceptionHandling/Handlers/IHttpExceptionHandler.cs
@@ -5,7 +5,7 @@ namespace Audacia.ExceptionHandling.Handlers
     /// <summary>
     /// A generic-less interface for <see cref="IHttpExceptionHandler"/>.
     /// </summary>
-    public interface IHttpExceptionHandler
+    public interface IHttpExceptionHandler : IExceptionHandler
     {
         /// <summary>
         /// Gets the status code to return for the given exception type.
